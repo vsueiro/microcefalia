962d58b6db4a217012d702a2647d6fb86b17115f
Vinicius Sueiro <vinicius.sueiro@estadao.com>
2016-08-25 02:26:55 -0300

    Mesclei o JSON mais limpo com as coordenadas para cada cidade (funcionou apenas excluir o último dígito do id IBGE). Também organizei o tratamento de dados numa função que recebe 2 parâmetros: URL do JSON (SAGE) e formato desejado (json ou csv). O CSV é só um teste e, portanto, traz apenas a última semana de cada município. Preciso fazer um novo tratamento que retire os valores zerados e também os acumulados, isto é, deixar apenas o que aconteceu naquela semana, por exemplo: semana 10: + 2 investigados e +1 confirmado; semana 11: +4 descartados etc.
    O melhor dos mundos seria converter o que fiz em javascript para python ou outra linguagem que pudesse rodar no servidor.

--- js/app.js
+++ js/app.js
@@ -1,141 +1,162 @@
-// $.getJSON( caminho + 'data/lista-microcefalia-2016-08-21.json', function( resultado ) {
-//     $.each( resultado, function( chave, municipio ) {
-//       console.log( municipio );
-//     });
-// });
+var amount = 0;
 
+function cleanCSV( data ) {
 
-// checar se a lista de municípios com coordenadas contém duplicatas
-var coordenadasUnicas = [];
+  var csv = 'id,nome,latitude,longitude,ano,sem,ta<br>';
 
-$.getJSON( caminho + 'data/br-localidades-2010-v1.json', function( cidades ) {
+  $.each( data, function( i, m ) {
 
-  $.each( cidades, function( i, cidade ) {
+    var casoMaisRecente = m.casos[ m.casos.length - 1 ];
 
-    if ( $.inArray( cidade[ 'id-ibge' ], coordenadasUnicas) > -1 ) { // se já consta o ID do município no array de únicos
+    var ano = casoMaisRecente.ano;
+    var sem = casoMaisRecente.sem;
+    var ta  = casoMaisRecente.ta;    
 
-      console.log( 'repetido: ' + cidade[ 'id-ibge' ] );
+    if ( ta > 0 ) {
 
-    } else { // se não consta
-
-      coordenadasUnicas.push( cidade[ 'id-ibge' ] );
+      csv += m[ 'id' ] + ',' + m.nome + ',' + m.coordenadas.lat + ',' + m.coordenadas.lon + ',' + ano + ',' + sem + ',' + ta + '<br>';
+      amount++;
 
     }
-  
+
   });
 
-});
+  console.log( 'csv retornou ' + amount + ' municípios' );
 
+  return csv; 
 
+}
 
+function clean( json, format ) {
 
+  console.log( 'Carregando JSON de coordenadas...' );
 
+  var municipiosCoordenadas;
 
+  $.getJSON( caminho + 'data/br-localidades-2010-v1.json', function( dados ) {
 
+    municipiosCoordenadas = dados;
 
+  });
 
+  console.log( 'Carregandon JSON com casos de microcefalia e zika...' );  
 
-var municipiosUnicos = [];
-var municipios = [];
-var reali = 0;
+  var municipiosUnicos = [];
+  var municipios = [];
+  var reali = 0;
 
-$.getJSON( caminho + 'data/lista-microcefalia-2016-08-21.json', function( resultado ) {
+  $.getJSON( json, function( resultado ) {
 
-    $.each( resultado.resultset, function( i, dados ) {
+      $.each( resultado.resultset, function( i, dados ) {
 
-      var id = parseInt( dados[0] );
+        var id = parseInt( dados[0] );
 
-      if ( $.inArray( id, municipiosUnicos) > -1 ) { // se já consta o ID do município no array de únicos
+        if ( $.inArray( id, municipiosUnicos) > -1 ) {
+        // se já consta o ID do município no array de únicos
 
-        var caso = { 
+          var caso = { 
 
-          "ano"  : parseInt( dados[4] ),
-          "sem"  : parseInt( dados[3] ),
-          "ta" : parseInt( dados[5] ),
-          "ti" : parseInt( dados[6] ),
-          "tc" : parseInt( dados[7] ),
-          "td" : parseInt( dados[8] ),
-          "ton" : parseInt( dados[9] ),
-          "toi" : parseInt( dados[10] ),
-          "toc" : parseInt( dados[11] ),
-          "tod" : parseInt( dados[12] )
+            "ano" : parseInt( dados[4]  ),
+            "sem" : parseInt( dados[3]  ),
+            "ta"  : parseInt( dados[5]  ),
+            "ti"  : parseInt( dados[6]  ),
+            "tc"  : parseInt( dados[7]  ),
+            "td"  : parseInt( dados[8]  ),
+            "ton" : parseInt( dados[9]  ),
+            "toi" : parseInt( dados[10] ),
+            "toc" : parseInt( dados[11] ),
+            "tod" : parseInt( dados[12] )
+
+          }
+
+          municipios[reali - 1][ 'casos' ].push( caso );
+
+        } else { // se não consta
+
+          municipiosUnicos.push( id );
+
+          municipios[reali] = {
+
+            "id"      : id,
+            "nome"    : dados[2],
+            "casos"   : []
+
+          };
+
+          var caso = {
+
+            "ano" : parseInt( dados[4]  ),
+            "sem" : parseInt( dados[3]  ),
+            "ta"  : parseInt( dados[5]  ),
+            "ti"  : parseInt( dados[6]  ),
+            "tc"  : parseInt( dados[7]  ),
+            "td"  : parseInt( dados[8]  ),
+            "ton" : parseInt( dados[9]  ),
+            "toi" : parseInt( dados[10] ),
+            "toc" : parseInt( dados[11] ),
+            "tod" : parseInt( dados[12] )
+
+          }
+
+          municipios[reali][ 'casos' ].push( caso );
+
+          lasti = i;
+          reali++;
 
         }
 
-        municipios[reali - 1][ 'casos' ].push( caso );
+      });
 
-      } else { // se não consta
+      console.log( 'Mesclando as duas bases de dados...' );  
 
-        municipiosUnicos.push( id );
+      $.each( municipios, function( i, municipio ) {
 
-        municipios[reali] = {
+        var found = false; 
 
-          "id"      : id,
-          "nome"    : dados[2],
-          "casos"   : []
+        $.each( municipiosCoordenadas, function( j, municipiosCoordenada ) {
 
-        };
+          var trimId = '';
+              trimId += municipiosCoordenada[ 'id-ibge' ];
+              trimId = trimId.slice(0, -1); // remove último dígito
 
-        var caso = {
 
-          "ano"  : parseInt( dados[4] ),
-          "sem"  : parseInt( dados[3] ),
-          "ta" : parseInt( dados[5] ),
-          "ti" : parseInt( dados[6] ),
-          "tc" : parseInt( dados[7] ),
-          "td" : parseInt( dados[8] ),
-          "ton" : parseInt( dados[9] ),
-          "toi" : parseInt( dados[10] ),
-          "toc" : parseInt( dados[11] ),
-          "tod" : parseInt( dados[12] )
+          if ( municipio[ 'id' ] == trimId ) {
+
+            municipio[ 'coordenadas' ] = {
+
+              "lat" : municipiosCoordenada[ 'latitude'  ],
+              "lon" : municipiosCoordenada[ 'longitude' ]
+
+            };
+
+            found = true;
+
+          }
+
+        });
+
+        if ( !found ) {
+
+          console.log( 'não encontrado: ' + municipio[ 'id' ] );
 
         }
 
-        municipios[reali][ 'casos' ].push( caso );
+      });
 
-        lasti = i;
-        reali++;
+      if ( format == 'json' ) {
+      
+        $( 'body' ).html( JSON.stringify( municipios ) );
 
       }
 
-    });
+      if ( format == 'csv'  ) {
 
-    $( 'body' ).html( JSON.stringify( municipios ) );
+        $( 'body' ).html( cleanCSV( municipios ) );
 
-});
+      }
 
+  });
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-$.each( UFs , function( i, uf ) {
-  $( 'body' ).append( uf[ 'sigla'] + ', ' + uf[ 'id' ] + ', ' + uf[ 'nome' ] + '<br>' );
-});
-
-$( 'body' ).append( '<hr>' );
-
-$.getJSON( caminho + 'data/codigo-dos-municipios.json', function( resultado ) {
-    $.each( resultado, function( indice, municipio ) {
-      markup += municipio[ 'UF' ] + ', ' + municipio[ 'Código' ] + ', ' + municipio[ 'Nome' ] + '<br>';
-    });
-    $( 'body' ).append( markup );
-});
-
-$.getJSON( caminho + 'data/lista-microcefalia-2016-08-21.json', function( resultado ) {
-    $.each( resultado, function( indice, municipio ) {
-      // pegar os dados aqui
-    });
-});
-*/
+clean( caminho + 'data/lista-microcefalia-2016-08-21.json', 'csv' );
