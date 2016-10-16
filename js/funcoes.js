7c490dc377d60364bfdc4b67992c126afe5bf3d7
Vinicius Sueiro <vinicius.sueiro@estadao.com>
2016-10-16 19:31:26 -0200

    Adicionado: página "organizando.php" para testar nova organização do código

--- js/funcoes.js
+++ js/funcoes.js
@@ -678,3 +678,19 @@ function baixar( id, nome ) {
     a.click();
     
 }
+
+carregaMapa();
+
+$( document ).on( 'change', '.semanas, .categorias', function() {
+
+  atualizaCirculos( semanaAtual(), categoriaAtual() );
+
+  deslizador.setStep( $( 'select.semanas option:selected' ).index(), 0 );
+
+  if ( visualizacao.data( 'ficha' ) ) {
+
+    mostraFicha( visualizacao.data( 'municipio' ), semanaAtual(), categoriaAtual() );
+
+  }
+
+});
