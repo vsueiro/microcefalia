24e90e2388f08faadb7e35922f70643cf2ebff47
Vinicius Sueiro <hi@vsueiro.com>
2016-12-23 02:03:43 -0200

    Modificado: gerador de dos ícones em Scss para separar o nome do ícone de sua cor. Por exemplo, “<i data-icon="play-medium"></i>” agora tem que ser escrito “<i data-icon="play" data-color="medium"></i>”

--- new/img/icon/scss-generator.php
+++ new/img/icon/scss-generator.php
@@ -4,11 +4,13 @@
 
   foreach ( glob( 'svg/*.svg' ) as $path ) {
 
-    $svg = file_get_contents( $path, FILE_USE_INCLUDE_PATH );
-    $file = basename( $path, '.svg' );
-
-    $scss .= 'i[data-icon="' . $file . '"]:before { background-image: svg-url(\'' . $svg . '\') }' . "\n";
+    $svg   = file_get_contents( $path, FILE_USE_INCLUDE_PATH );
+    $file  = basename( $path, '.svg' );
+    $arr   = preg_split( '/\-(?=[^-]*$)/', $file ); // split by last -
+    $icon  = $arr[ 0 ];
+    $color = $arr[ 1 ];
+    $scss .= 'i[data-icon="' . $icon . '"][data-color="' . $color . '"]:before { background-image: svg-url(\'' . $svg . '\') }' . "\n";
 
   }
-
+  echo $scss;
   file_put_contents( '../../scss/icons.scss', $scss );
